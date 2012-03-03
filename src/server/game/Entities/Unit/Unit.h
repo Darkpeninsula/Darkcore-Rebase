d5363e98d695882fc244db9449b07e15074aead7
Nevan <site@darkpeninsula.eu>
2012-03-03 11:27:06 +0100

    Implement SPELL_AURA_CAST_WHILE_WALKING

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1910,6 +1910,8 @@ class Unit : public WorldObject
         // delayed+channeled spells are always interrupted
         void InterruptNonMeleeSpells(bool withDelayed, uint32 spellid = 0, bool withInstant = true);
 
+        bool CanCastWhileWalking(const SpellEntry * const sp);
+
         Spell* GetCurrentSpell(CurrentSpellTypes spellType) const { return m_currentSpells[spellType]; }
         Spell* GetCurrentSpell(uint32 spellType) const { return m_currentSpells[spellType]; }
         Spell* FindCurrentSpellBySpellId(uint32 spell_id) const;
